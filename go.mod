9acbfcfd9ae2fee2679ba8a055941dc4abf5e2f6
Sergei Razmetov <razmser@gmail.com>
2024-10-23 11:30:44 +0300

    feat: initial create insert select implementation

---
+++ go.mod
@@ -0,0 +1,28 @@
+module github.com/razmser/ch-exp
+
+go 1.21.13
+
+require (
+	github.com/ClickHouse/ch-go v0.63.1 // indirect
+	github.com/dmarkham/enumer v1.5.10 // indirect
+	github.com/go-faster/city v1.0.1 // indirect
+	github.com/go-faster/errors v0.7.1 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/hashicorp/go-version v1.7.0 // indirect
+	github.com/klauspost/compress v1.17.11 // indirect
+	github.com/pascaldekloe/name v1.0.1 // indirect
+	github.com/pierrec/lz4/v4 v4.1.21 // indirect
+	github.com/segmentio/asm v1.2.0 // indirect
+	go.opentelemetry.io/otel v1.24.0 // indirect
+	go.opentelemetry.io/otel/metric v1.24.0 // indirect
+	go.opentelemetry.io/otel/trace v1.24.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/mod v0.13.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/tools v0.14.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+
+	"github.com/ClickHouse/ch-go"
+	"github.com/ClickHouse/ch-go/proto"
+)
+
+func createTablesCh(ctx context.Context, conn *ch.Client) {
+	if err := conn.Do(ctx, ch.Query{
+		Body: `CREATE TABLE IF NOT EXISTS exp
+(
+    id Int32,
+    value AggregateFunction(uniq, Int32)
+)
+ENGINE = MergeTree
+ORDER BY id`,
+	}); err != nil {
+		panic(err)
+	}
+
+	if err := conn.Do(ctx, ch.Query{
+		Body: `CREATE TABLE IF NOT EXISTS exp_input
+(
+    id Int32,
+    value Int32
+)
+ENGINE = Null`,
+	}); err != nil {
+		panic(err)
+	}
+
+	if err := conn.Do(ctx, ch.Query{
+		Body: `CREATE MATERIALIZED VIEW IF NOT EXISTS exp_mv TO exp AS
+SELECT id, uniqState(value) AS value
+FROM exp_input
+GROUP BY id`,
+	}); err != nil {
+		panic(err)
+	}
+}
+
+func insertCh(ctx context.Context, conn *ch.Client) {
+	// Define all columns of table.
+	var (
+		id    proto.ColInt32
+		value proto.ColInt32
+	)
+
+	// Append 10 rows to initial data block.
+	id.Append(1)
+	value.Append(1)
+	id.Append(1)
+	value.Append(2)
+	id.Append(2)
+	value.Append(2)
+	id.Append(2)
+	value.Append(3)
+
+	// Insert single data block.
+	input := proto.Input{
+		{Name: "id", Data: id},
+		{Name: "value", Data: value},
+	}
+	if err := conn.Do(ctx, ch.Query{
+		Body: "INSERT INTO exp_input VALUES",
+		// Or "INSERT INTO test_table_insert (ts, severity_text, severity_number, body, name, arr) VALUES"
+		Input: input,
+	}); err != nil {
+		panic(err)
+	}
+}
+
+func selectCh(ctx context.Context, conn *ch.Client) {
+	var (
+		id    proto.ColInt32
+		value proto.ColUInt64
+	)
+	if err := conn.Do(ctx, ch.Query{
+		Body: "SELECT id, uniqMerge(value) AS value FROM exp GROUP BY id",
+		Result: proto.Results{
+			{Name: "id", Data: &id},
+			{Name: "value", Data: &value},
+		},
+	}); err != nil {
+		panic(err)
+	}
+	fmt.Println("id\tvalue")
+	for i := range id {
+		fmt.Printf("%d\t%d\n", id[i], value[i])
+	}
+}
+
+func main() {
+	ctx := context.Background()
+
+	slog.Info("connecting...")
+	conn, err := ch.Dial(ctx, ch.Options{})
+	if err != nil {
+		panic(err)
+	}
+	slog.Info("connected")
+
+	slog.Info("creating table...")
+	createTablesCh(ctx, conn)
+	slog.Info("table created")
+
+	slog.Info("inserting...")
+	insertCh(ctx, conn)
+	slog.Info("inserted")
+
+	slog.Info("selecting...")
+	selectCh(ctx, conn)
+	slog.Info("selected")
+}
